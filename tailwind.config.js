148ca3790f0ff75effff5fffda79d0b13ccde8f1
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-27 00:38:11 -0500

    Configuración de la fuente, colores, linkeos a los estilos y código javascript

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        first: "#345FF6",
+        second: "#253347",
+        third: "#5E6E85",
+        fourth: "#D8E2E7",
+        fifth: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
