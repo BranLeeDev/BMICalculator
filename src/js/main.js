13d16db92098a2d943614f99926a51644a34bb8d
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-28 11:35:08 -0500

    Agregue la lógica en código para mostrar al usuario el resultado de su BMI y el rango ideal de su peso

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import "../css/index.css";
 
 const valuesAllowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const inputsText = document.querySelectorAll('input[type="text"]');
+const resultBMI = document.getElementById("result-bmi");
 const isNumber = (value) => {
   return valuesAllowed.some((number) => number.includes(value.at(-1)));
 };
@@ -12,6 +13,20 @@ function validateInputContent() {
   function inputTypeText(input) {
     input.addEventListener("input", (e) => {
       const currentValue = e.target.value;
+
+      if (!inputsText[0].value && !inputsText[1].value) {
+        resultBMI.firstElementChild.firstElementChild.textContent = "Welcome";
+        resultBMI.firstElementChild.firstElementChild.classList.remove("text-base");
+        resultBMI.firstElementChild.firstElementChild.classList.add("text-2xl");
+        resultBMI.firstElementChild.lastElementChild.textContent = "";
+
+        resultBMI.lastElementChild.textContent =
+          "Enter your height and weight and you'll see your BMI result here";
+        const spanWeightRange = document.createElement("span");
+        spanWeightRange.textContent = "";
+        resultBMI.lastElementChild.appendChild(spanWeightRange);
+        return;
+      }
 
       if (!isNumber(currentValue)) {
         const previosValue = currentValue.at(-2);
@@ -32,10 +47,62 @@ function validateInputContent() {
       if (currentValue.split("").length >= 4) {
         const result = e.target.value.split("");
         e.target.value = result.slice(0, -1).join("");
+        return;
+      }
+
+      if (inputsText[0].value && inputsText[1].value) {
+        const height = +inputsText[0].value / 100;
+        const weight = +inputsText[1].value;
+        const BMI = (weight / height ** 2).toFixed(1);
+        const weightRange = `${(height * 100 - 100 - (height * 100 - 150) / 2.5 - 7.7).toFixed(
+          1
+        )}kgs - ${(height * 100 - 100 - (height * 100 - 150) / 4 + 8.9).toFixed(1)}kgs.`;
+        if (height >= 2.8 || weight >= 600 || height <= 0.7 || weight <= 20) return;
+        resultBMI.classList.remove("gap-4");
+        resultBMI.classList.add("gap-6");
+        resultBMI.firstElementChild.firstElementChild.textContent = "Your BMI is...";
+        resultBMI.firstElementChild.firstElementChild.classList.remove("text-2xl");
+        resultBMI.firstElementChild.firstElementChild.classList.add("text-base");
+        resultBMI.firstElementChild.lastElementChild.textContent = BMI;
+
+        const spanWeightRange = document.createElement("span");
+
+        switch (true) {
+          case BMI < 18.5:
+            resultBMI.lastElementChild.textContent =
+              "Your BMI suggests you are underweight. Your ideal weight may be higher, ideally between ";
+            spanWeightRange.textContent = weightRange;
+            resultBMI.lastElementChild.appendChild(spanWeightRange);
+            resultBMI.lastElementChild.firstElementChild.classList.add("font-semibold");
+            break;
+          case BMI >= 18.5 && BMI < 24.9:
+            resultBMI.lastElementChild.textContent =
+              "Your BMI suggests you’re a healthy weight. Your ideal weight is between ";
+            spanWeightRange.textContent = weightRange;
+            resultBMI.lastElementChild.appendChild(spanWeightRange);
+            resultBMI.lastElementChild.firstElementChild.classList.add("font-semibold");
+            break;
+          case BMI >= 24.9 && BMI < 29.9:
+            resultBMI.lastElementChild.textContent =
+              "Your BMI suggests you are overweight. Your ideal weight may be lower, ideally between ";
+            spanWeightRange.textContent = weightRange;
+            resultBMI.lastElementChild.appendChild(spanWeightRange);
+            resultBMI.lastElementChild.firstElementChild.classList.add("font-semibold");
+            break;
+          case BMI >= 30:
+            resultBMI.lastElementChild.textContent =
+              "Your BMI suggests you are obese. Your ideal weight may be lower, ideally between ";
+            spanWeightRange.textContent = weightRange;
+            resultBMI.lastElementChild.appendChild(spanWeightRange);
+            resultBMI.lastElementChild.firstElementChild.classList.add("font-semibold");
+        }
+        return;
       }
     });
   }
 }
+
+function bmiCalculateResult() {}
 
 function main() {
   validateInputContent();
