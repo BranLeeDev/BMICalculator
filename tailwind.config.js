631a5d0d34e5a9cde24bf07dc940cc7d538cea00
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-27 19:41:19 -0500

    Estilos añadidos para el responsive en mobile, se hizó algunas modificaciones al html

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
         third: "#5E6E85",
         fourth: "#D8E2E7",
         fifth: "#FFFFFF",
+        "input-number": "#c9ccD1",
+      },
+      boxShadow: {
+        "3xl": "1rem 2rem 3.5rem 0rem rgba(143, 174, 207, 0.25)",
       },
     },
   },
