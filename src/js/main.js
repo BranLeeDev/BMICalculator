4da1ae1e2a21c699a9cf43b43748c3b90cc133f2
Brandon <brandonaguerodeveloper@gmail.com>
2023-09-27 21:10:39 -0500

    Agregué un margitop a la sección de 'limitation of BMI'. Además añadí código javascript para asegurarnos de que el usuario solo ingrese números

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,37 @@
 import "../css/index.css";
+
+const valuesAllowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const inputsText = document.querySelectorAll('input[type="text"]');
+const isNumber = (value) => {
+  return valuesAllowed.some((number) => number.includes(value.at(-1)));
+};
+
+function validateInputContent() {
+  inputsText.forEach(inputTypeText);
+
+  function inputTypeText(input) {
+    input.addEventListener("input", (e) => {
+      const currentValue = e.target.value;
+      if (!isNumber(currentValue)) {
+        const previosValue = currentValue.at(-2);
+        if (!previosValue) {
+          e.target.value = "";
+          return;
+        }
+
+        const lastValue = currentValue.at(-1);
+        if (!isNumber(lastValue)) {
+          const result = e.target.value.split("");
+          const index = result.findIndex((item) => item === lastValue);
+          e.target.value = result.splice(0, index).join("");
+        }
+      }
+    });
+  }
+}
+
+function main() {
+  validateInputContent();
+}
+
+window.addEventListener("load", main);
